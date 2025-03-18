1a918d19183cd3df2e65d50852fa30186a995347
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-03-18 07:37:25 +0000

    test

--- terraform/modules/eks-cluster/outputs.tf
+++ terraform/modules/eks-cluster/outputs.tf
@@ -1,29 +1,34 @@
 output "cluster_id" {
   description = "The ID of the EKS cluster"
-  value       = aws_eks_cluster.eks_cluster.id
+  value       = aws_eks_cluster.this.id
 }
 
 output "cluster_endpoint" {
   description = "The endpoint for the EKS cluster"
-  value       = aws_eks_cluster.eks_cluster.endpoint
+  value       = aws_eks_cluster.this.endpoint
 }
 
 output "cluster_arn" {
   description = "The ARN of the EKS cluster"
-  value       = aws_eks_cluster.eks_cluster.arn
+  value       = aws_eks_cluster.this.arn
 }
 
 output "cluster_version" {
   description = "The Kubernetes version of the EKS cluster"
-  value       = aws_eks_cluster.eks_cluster.version
+  value       = aws_eks_cluster.this.version
 }
 
 output "node_group_arn" {
   description = "The ARN of the EKS node group"
-  value       = aws_eks_node_group.eks_node_group.arn
+  value       = aws_eks_node_group.this.arn
 }
 
 output "node_group_id" {
   description = "The ID of the EKS node group"
-  value       = aws_eks_node_group.eks_node_group.id
+  value       = aws_eks_node_group.this.id
+}
+
+output "alb_dns_name" {
+  description = "The DNS name of the ALB"
+  value       = aws_lb.this.dns_name
 }
--- terraform/modules/eks-cluster/variables.tf
+++ terraform/modules/eks-cluster/variables.tf
@@ -1,3 +1,4 @@
+
 variable "cluster_name" {
   description = "The name of the EKS cluster"
   type        = string
@@ -6,6 +7,11 @@ variable "cluster_name" {
 variable "vpc_id" {
   description = "The VPC ID where the EKS cluster will be deployed"
   type        = string
+}
+
+variable "subnets" {
+  description = "A list of subnet IDs where the EKS cluster will be deployed"
+  type        = list(string)
 }
 
 variable "subnet_ids" {
