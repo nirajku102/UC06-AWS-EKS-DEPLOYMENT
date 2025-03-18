b122112ea265e985ce55f7f0e2b6118608a57e6c
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-03-18 07:14:11 +0000

    Adding lock

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,39 @@
-
 output "vpc_id" {
   description = "The ID of the VPC"
   value       = module.vpc.vpc_id
 }
 
-output "ecs_cluster_id" {
-  description = "The ID of the ECS cluster"
-  value       = module.ecs_cluster.ecs_cluster_id
+output "eks_cluster_id" {
+  description = "The ID of the EKS cluster"
+  value       = module.eks_cluster.cluster_id
+}
+
+output "eks_cluster_endpoint" {
+  description = "The endpoint for the EKS cluster"
+  value       = module.eks_cluster.cluster_endpoint
+}
+
+output "eks_cluster_arn" {
+  description = "The ARN of the EKS cluster"
+  value       = module.eks_cluster.cluster_arn
+}
+
+output "eks_cluster_version" {
+  description = "The Kubernetes version of the EKS cluster"
+  value       = module.eks_cluster.cluster_version
+}
+
+output "node_group_arn" {
+  description = "The ARN of the EKS node group"
+  value       = module.eks_cluster.node_group_arn
+}
+
+output "node_group_id" {
+  description = "The ID of the EKS node group"
+  value       = module.eks_cluster.node_group_id
 }
 
 output "alb_dns_name" {
   description = "The DNS name of the ALB"
-  value       = module.ecs_cluster.alb_dns_name
+  value       = aws_lb.this.dns_name
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,10 @@
 
 terraform {
   backend "s3" {
-    bucket         = "fargate-state-bucket-uc3"
+    bucket         = "eks-state-bucket-uc06"
     key            = "terraform"
     region         = "eu-west-2"
     encrypt        = true
+    dynamodb_table = "terraform-lock-table"
   }
 }
